003d8800e72aafa75ef6a9d72e0d768ec4bfb3cb
ivan-pl <pleskaiv@gmail.com>
2022-09-19 17:26:37 +0300

    layout: add about page

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require("path");
 
-const pages = ["index", "articles", "topic-1"];
+const pages = ["index", "articles", "topic-1", "about"];
 
 module.exports = {
   entry: pages.reduce((config, page) => {
