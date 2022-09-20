c5edc159e9848d26ff3055c7439882ac68af6796
ivan-pl <pleskaiv@gmail.com>
2022-09-20 23:06:41 +0300

    feat: add options to Carousel

--- src/library/carousel.js
+++ src/library/carousel.js
@@ -3,7 +3,13 @@ export default class Carousel {
 
   currentSlideInd = 0;
 
-  constructor(element) {
+  intervalTimer = null;
+
+  constructor(
+    element,
+    { gap = "5%", duration = "0.5s", interval = null } = {}
+  ) {
+    this.options = { gap, duration, interval };
     this.carousel = element;
   }
 
@@ -11,6 +17,25 @@ export default class Carousel {
     this.carousel.classList.add("carousel");
     this.#initializeSlides();
     this.#addButtons();
+    this.#setOptions();
+  }
+
+  #startAutoScroll() {
+    const clickEvent = new Event("click");
+    this.intervalTimer = setInterval(() => {
+      this.rightArrow.dispatchEvent(clickEvent);
+    }, this.options.interval);
+  }
+
+  #setOptions() {
+    this.carousel.style.gap = this.options.gap;
+    this.carousel.querySelector(".carousel__slide").style[
+      "transition-duration"
+    ] = this.options.duration;
+
+    if (this.options.interval !== null) {
+      this.#startAutoScroll();
+    }
   }
 
   #initializeSlides() {
@@ -25,6 +50,11 @@ export default class Carousel {
     function move(direction) {
       this.currentSlideInd += direction;
 
+      if (this.intervalTimer) {
+        clearInterval(this.intervalTimer);
+        this.#startAutoScroll();
+      }
+
       if (this.currentSlideInd >= this.slides.length) {
         this.slides.forEach((slide) => {
           slide.style.transform = "translateX(0)";
@@ -36,14 +66,20 @@ export default class Carousel {
 
       if (this.leftArrow.style.display === "none" && this.currentSlideInd > 0) {
         this.leftArrow.style.display = "block";
+      } else if (this.currentSlideInd === 0) {
+        this.leftArrow.style.display = "none";
       }
 
       this.slides.forEach((slide) => {
-        slide.style.transform = `translateX(-${this.currentSlideInd * 102}%)`;
+        const translateX =
+          this.currentSlideInd * 100 +
+          this.currentSlideInd * parseFloat(this.options.gap);
+        slide.style.transform = `translateX(-${translateX}%)`;
       });
     }
 
     this.leftArrow = this.#createButton("left-arrow", move.bind(this, -1));
+    this.leftArrow.style.display = "none";
     this.rightArrow = this.#createButton("right-arrow", move.bind(this, 1));
   }
 
--- src/topic-1.js
+++ src/topic-1.js
@@ -5,5 +5,13 @@ import "./pages/topic-1.html";
 import "./library/carousel.scss";
 import Carousel from "./library/carousel";
 
-const carousel = new Carousel(document.querySelector(".article__carousel"));
-carousel.initialize();
+function initializeCarousel() {
+  const options = { gap: "7%", duration: "0.7s", interval: "10000" };
+  const carousel = new Carousel(
+    document.querySelector(".article__carousel"),
+    options
+  );
+  carousel.initialize();
+}
+
+initializeCarousel();
