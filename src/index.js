76c800e53eeed6c53ac7a898f9722fa72b564250
ivan-pl <pleskaiv@gmail.com>
2022-09-20 21:04:06 +0300

    layout: add gallery into topic-1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import "normalize.css";
 import "./styles/fonts.css";
 import "./styles/main.scss";
 import "./pages/index.html";
-
-import "./library/carousel.scss";
-import Carousel from "./library/carousel";
-
-const carousel = new Carousel(document.querySelector(".main__carousel"));
-carousel.initialize();
--- src/topic-1.js
+++ src/topic-1.js
@@ -2,3 +2,8 @@ import "normalize.css";
 import "./styles/fonts.css";
 import "./styles/topic-1.scss";
 import "./pages/topic-1.html";
+import "./library/carousel.scss";
+import Carousel from "./library/carousel";
+
+const carousel = new Carousel(document.querySelector(".article__carousel"));
+carousel.initialize();
