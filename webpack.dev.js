008b1bee926ad1bc8731c89dc409e0a529b126b2
ivan-pl <pleskaiv@gmail.com>
2022-09-26 09:25:02 +0300

    build: add devServer config

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,9 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    static: "./dist",
+    compress: true,
+    port: 9000,
+    open: true,
+    watchFiles: ["*.html"],
   },
 });
